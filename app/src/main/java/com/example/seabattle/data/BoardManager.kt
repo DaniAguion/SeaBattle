0b01b7975cd90c9fa692c2663dea0c4777ab9823
DaniAguion <daniaguion@gmail.com>
2025-04-23 14:01:27 +0200

    Implemented cleaner code to set ship position

--- app/src/main/java/com/example/seabattle/data/BoardManager.kt
+++ app/src/main/java/com/example/seabattle/data/BoardManager.kt
@@ -12,13 +12,23 @@ class BoardManager {
     val ships: List<Ship>
 
     init {
+        ships = seedGameBoard()
+    }
+
+
+    fun getGameBoard(): GameBoard {
+        return gameBoard
+    }
+
+
+    private fun seedGameBoard() : List<Ship> {
         val shipSizes = listOf(5, 4, 3, 3, 2)
-        ships = shipSizes.map { size -> Ship(size, setPosition(size)) }
+        return shipSizes.map { size -> Ship(size, setPosition(size)) }
     }
 
 
     // Function to set the position of a ship on the game board
-    fun setPosition(shipSize: Int) : Position {
+    private fun setPosition(shipSize: Int) : Position {
         val position = definePosition(shipSize)
         for (i in 0 until shipSize)
             when (position.shipDirection) {
@@ -30,43 +40,38 @@ class BoardManager {
 
 
     // Function to define a random position for a ship on the game board
-    fun definePosition(shipSize: Int): Position {
+    private fun definePosition(shipSize: Int): Position {
         while (true) {
             val shipDirection = ShipDirection.entries.random()
-            var x: Int
-            var y: Int
+            var x0: Int
+            var y0: Int
+            var x1: Int
+            var y1: Int
 
             when (shipDirection){
                 ShipDirection.VERTICAL -> {
-                    x = (0 until gameBoardSize).random()
-                    y = (0 until gameBoardSize - shipSize).random()
-
-                    if (checkPosition(gameBoard, x0 = x, x1 = x, y0 = y, y1 = y + shipSize)) {
-                        return Position(x, y, shipDirection)
-                    }
+                    x0 = (0 until gameBoardSize).random()
+                    y0 = (0 until gameBoardSize - shipSize).random()
+                    x1 = x0
+                    y1 = y0 + shipSize
                 }
                 ShipDirection.HORIZONTAL -> {
-                    x = (0 until gameBoardSize - shipSize).random()
-                    y = (0 until gameBoardSize).random()
-
-                    if (checkPosition(gameBoard, x0 = x, x1 = x + shipSize, y0 = y, y1 = y)) {
-                        return Position(x, y, shipDirection)
-                    }
+                    x0 = (0 until gameBoardSize - shipSize).random()
+                    y0 = (0 until gameBoardSize).random()
+                    x1 = x0 + shipSize
+                    y1 = y0
                 }
+            }
+
+            if (checkPosition(x0, x1, y0, y1)) {
+                return Position(x0, y0, shipDirection)
             }
         }
     }
 
 
     // Function to check if a ship can be placed at the given position
-    fun checkPosition(
-        gameBoard: GameBoard,
-        x0: Int,
-        x1: Int,
-        y0: Int,
-        y1: Int,
-    ): Boolean {
-
+    private fun checkPosition(x0: Int, x1: Int, y0: Int, y1: Int): Boolean {
         // Extend the area to ensure that the ship is not placed next to another ship
         val xMin = (x0 - 1).coerceAtLeast(0)
         val xMax = (x1 + 1).coerceAtMost(gameBoardSize - 1)
@@ -75,9 +80,7 @@ class BoardManager {
 
         for (x in xMin..xMax) {
             for (y in yMin .. yMax) {
-                if (gameBoard.cells[x][y] != 0) {
-                    return false
-                }
+                if (gameBoard.cells[x][y] != 0) return false
             }
         }
         return true
@@ -90,10 +93,6 @@ class BoardManager {
             1 -> gameBoard.cells[x][y] = 3
             else -> gameBoard.cells[x][y] = gameBoard.cells[x][y]
         }
-        return gameBoard
-    }
-
-    fun getGameBoard(): GameBoard {
         return gameBoard
     }
 }
