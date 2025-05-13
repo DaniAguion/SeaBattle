d8e2c1d719d7bcec9100898653c25144b3e2675a
DaniAguion <daniaguion@gmail.com>
2025-05-13 16:33:31 +0200

    Improved presentation of player in Game Screen

--- app/src/main/java/com/example/seabattle/presentation/screens/game/GameScreen.kt
+++ app/src/main/java/com/example/seabattle/presentation/screens/game/GameScreen.kt
@@ -1,12 +1,14 @@
 package com.example.seabattle.presentation.screens.game
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,8 +17,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,7 +42,6 @@ fun GameScreen(
 
     GameScreenContent(
         modifier = modifier,
-        navController = navController,
         game = gameUiState.game
     )
 }
@@ -48,108 +49,72 @@ fun GameScreen(
 @Composable
 fun GameScreenContent(
     modifier: Modifier,
-    navController: NavHostController,
     game: Game?
 ) {
-    Column(
+    LazyColumn(
         modifier = modifier
             .padding(dimensionResource(R.dimen.padding_medium))
             .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ){
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-            ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(dimensionResource(R.dimen.padding_small)),
-                    verticalAlignment = Alignment.CenterVertically
-                ){
-                    AsyncImage(
-                        model = game?.player1?.photoUrl,
-                        contentDescription = "User photo",
-                        modifier = Modifier.size(40.dp),
-                        contentScale = ContentScale.Crop
-                    )
-                    Text(
-                        text = "${game?.player1?.displayName}",
-                        fontSize = 16.sp,
-                        fontWeight = SemiBold,
-                        modifier = Modifier
-                            .padding(start = dimensionResource(R.dimen.padding_small))
-                    )
-                }
+        // Players
+        item {
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                CardPlayer(
+                    player = game?.player1
+                )
+                CardPlayer(
+                    player = game?.player2
+                )
             }
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-            ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(dimensionResource(R.dimen.padding_small)),
-                    verticalAlignment = Alignment.CenterVertically
-                ){
-                    AsyncImage(
-                        model = game?.player2?.photoUrl,
-                        contentDescription = "User photo",
-                        modifier = Modifier.size(40.dp),
-                        contentScale = ContentScale.Crop
-                    )
-                    Text(
-                        text = "${game?.player2?.displayName}",
-                        fontSize = 16.sp,
-                        fontWeight = SemiBold,
-                        modifier = Modifier
-                            .padding(start = dimensionResource(R.dimen.padding_small))
-                    )
-                }
-            }
-        }
-        Text(
-            text = "Game Screen",
-            modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
-        )
-        if (game != null) {
-            Text(
-                text = "Game ID: ${game.gameId}",
-                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
-            )
-            Text(
-                text = "State: ${game.gameState}",
-                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
-            )
-            Text(
-                text = "Player 1: ${game.player1.displayName}",
-                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
-            )
-            Text(
-                text = "Player 2: ${game.player2.displayName}",
-                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
-            )
         }
     }
 }
 
-        /*
-        Board(
-            gameBoard = gameBoard,
-            onCellClick = onCellClick,
-        )
-        */
+
 
 @Composable
-fun CardPlayer(){
+fun CardPlayer(
+    player: UserBasic?,
+){
+    Card {
+        Row(
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.padding_small))
+                .sizeIn(
+                    minWidth = 150.dp
+                ),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            if (player?.photoUrl.isNullOrEmpty()) {
+                Image(
+                    painter = painterResource(id = R.drawable.account_box_40px),
+                    contentDescription = "User photo",
+                    modifier = Modifier.size(40.dp)
+                )
+            } else {
+                AsyncImage(
+                    model = player.photoUrl,
+                    contentDescription = "User photo",
+                    modifier = Modifier.size(40.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
 
+            Text(
+                text = "${player?.displayName}",
+                fontSize = 16.sp,
+                fontWeight = SemiBold,
+                modifier = Modifier
+                    .padding(start = dimensionResource(R.dimen.padding_small))
+            )
+        }
+    }
 }
 
 
@@ -159,12 +124,11 @@ fun CardPlayer(){
 fun GameScreenPreview(){
     GameScreenContent(
         modifier = Modifier.fillMaxSize(),
-        navController = NavHostController(context = LocalContext.current),
         game = Game(
             gameId = "123",
-            player1 = UserBasic("userId", "displayName"),
+            player1 = UserBasic("userId", "Daniel"),
             player1Board = GameBoard().toMapOfMaps(),
-            player2 = UserBasic("userId", "displayName"),
+            player2 = UserBasic("userId", "PedroPablo80"),
             player2Board = GameBoard().toMapOfMaps(),
             gameState = GameState.IN_PROGRESS.name
         )
