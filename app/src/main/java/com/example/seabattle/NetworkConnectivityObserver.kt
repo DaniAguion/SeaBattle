f1b10e46e4d7c270117145a4e7508e1fe5340589
DaniAguion <daniaguion@gmail.com>
2025-06-27 02:40:23 +0200

    Trying to fix presence

---
+++ app/src/main/java/com/example/seabattle/NetworkConnectivityObserver.kt
@@ -0,0 +1,4 @@
+package com.example.seabattle
+
+class NetworkConnectivityObserver {
+}
---
+++ app/src/main/java/com/example/seabattle/domain/usecase/presence/ListenPresenceUseCase.kt
@@ -0,0 +1,41 @@
+package com.example.seabattle.domain.usecase.presence
+
+import com.example.seabattle.domain.SessionService
+import com.example.seabattle.domain.errors.DomainError
+import com.example.seabattle.domain.errors.PresenceError
+import com.example.seabattle.domain.repository.PresenceRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.collect
+import timber.log.Timber
+
+class SetPresenceUseCase (
+    private val presenceRepo: PresenceRepository,
+    private val sessionService: SessionService,
+    private val ioDispatcher: CoroutineDispatcher
+) {
+    suspend operator fun invoke(): Result<Unit> = withContext(ioDispatcher) {
+        runCatching {
+            val userId = sessionService.getCurrentUserId()
+            if (userId == "") {
+                throw PresenceError.UserNotAuthenticated()
+            }
+            presenceRepo.definePresence(userId = userId).getOrThrow()
+            presenceRepo.listenUserPresence(userId).collect { result ->
+                result.onSuccess { status ->
+                    Timber.d("User presence status is $status.")
+                }
+                .onFailure { error ->
+                    Timber.e(error, "Failed to listen user presence.")
+                }
+            }
+        }
+        .onFailure { e ->
+            Timber.e(e, "SetPresenceUseCase failed.")
+        }
+        .recoverCatching { throwable ->
+            if (throwable is PresenceError) throw throwable
+            else throw DomainError.Unknown(throwable)
+        }
+    }
+}
